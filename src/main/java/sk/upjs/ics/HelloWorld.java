a082871b1d15ea54a051056cf8f681d16bb66373
student <student@PC_katedra>
2016-09-20 07:57:38 +0200

    Pridane esteticke ciary na uvod a zaver vypisu

--- src/main/java/sk/upjs/ics/HelloWorld.java
+++ src/main/java/sk/upjs/ics/HelloWorld.java
@@ -2,8 +2,10 @@ package sk.upjs.ics;
 
 public class HelloWorld {
     public static void main(String[] args) {
+        System.out.println("--------------");
         for(int i = 0; i < 3; i++) {
             System.out.println("Ahoj");
         }
+        System.out.println("--------------");
     }
 }
