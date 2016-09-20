42d6ed80b1aeb4bd5340967d1c58e6aa9bf7609e
student <student@PC_katedra>
2016-09-20 08:07:15 +0200

    Pridane vykricniky k hlaske

--- src/main/java/sk/upjs/ics/HelloWorld.java
+++ src/main/java/sk/upjs/ics/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("--------------");
         for(int i = 0; i < 3; i++) {
-            System.out.println("Ahoj");
+            System.out.println("Ahoj!!!");
         }
         System.out.println("--------------");
     }
