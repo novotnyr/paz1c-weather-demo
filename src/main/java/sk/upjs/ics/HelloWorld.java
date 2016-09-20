c4ada697e6978f919507bdfa41acf0fbe4a4320d
Robert Novotny <robert.novotny@dcoresystems.com>
2016-09-20 07:59:53 +0200

    Zmeneny pocet hlasok na pat

--- src/main/java/sk/upjs/ics/HelloWorld.java
+++ src/main/java/sk/upjs/ics/HelloWorld.java
@@ -3,8 +3,8 @@ package sk.upjs.ics;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("--------------");
-        for(int i = 0; i < 3; i++) {
-            System.out.println("Ahoj");
+        for(int i = 0; i < 5; i++) {
+            System.out.println("Ahoj svet!");
         }
         System.out.println("--------------");
     }
